64a872931ccc20bfc1993aeb3cb7def1679cba70
Robin Qiu <574244038@qq.com>
2017-10-18 02:07:55 +0800

    修改input

--- src/page/login.vue
+++ src/page/login.vue
@@ -11,10 +11,10 @@
                     <h2>登录</h2>
                 </div>
                 <div class="input-field">
-                    <el-input v-model="usercode" placeholder="手机号" maxlength="11"></el-input>
+                    <el-input v-model="usercode" placeholder="手机号"></el-input>
                 </div>
                 <div class="input-field">
-                    <el-input type="password" v-model="password" placeholder="密码" maxlength="11" @keyup.13="login"></el-input>
+                    <el-input type="password" v-model="password" placeholder="密码" @keyup.13="login"></el-input>
                 </div>
                 <div class="action">
                     <div class="clearfix">
@@ -41,8 +41,6 @@
     // import '../assets/bootstrap/css/bootstrap.min.css'
     import '../css/reset.css'
     import '../css/zx.css'
-    import '../css/login.css'
-    // import 
     import {is_mobile,isNull} from '../js/util.js'
     import Api from '../api/api.js'
     let api = new Api();
@@ -109,7 +107,7 @@
     }
 </script>
 
-<style lang="less">
+<style lang="less" scope>
     .btn-block{
         display:block;
     }
@@ -122,5 +120,112 @@
     .wfull{
         width:100%;
         height:100%;
+    }
+    .wrapper {
+        position: relative;
+        background-image: url(../images/login_bg.jpg);
+        background-repeat: no-repeat;
+        background-position: top center;
+    }
+
+    .login-wrap,
+    .register-wrap {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 456px;
+        padding: 28px 50px 38px;
+        background-color: rgba(255, 255, 255, .6);
+        transform: translate(-50%, -50%);
+        -moz-transform: translate(-50%, -50%);
+        -webkit-transform: translate(-50%, -50%);
+    }
+    input[type='text'],
+    input[type='password'] {
+        display: block;
+        width: 100%;
+        background: #fff;
+        border: none;
+        outline: none;
+        padding: 11px 8px;
+        border-radius: 4px;
+    }
+
+    input[type='text']:focus,
+    input[type='password']:focus {
+        box-shadow: 0 0 1px 1px #ff8b6f;
+        -webkit-box-shadow: 0 0 1px 1px #ff8b6f;
+        -moz-box-shadow: 0 0 1px 1px #ff8b6f;
+    }
+
+    h2 {
+        font-size: 24px;
+        color: #ff8b6f;
+        text-align: center;
+    }
+
+    .input-field {
+        position: relative;
+        font-size: 16px;
+        margin-bottom: 14px;
+        color: #666;
+    }
+
+    .base-register .input-field {
+        margin-bottom: 14px;
+    }
+
+    .action {
+        margin-bottom: 14px;
+    }
+
+    .action .remember {
+        vertical-align: baseline;
+        margin-right: 4px;
+    }
+
+    .action label {
+        color: #999999;
+        font-weight: 400;
+        font-size: 16px;
+    }
+
+    .linelight {
+        font-size: 16px;
+        color: #ff8b6f;
+    }
+
+    .linelight:hover {
+        text-decoration: underline;
+    }
+
+    .relex {
+        margin-top: 15px;
+        line-height: 1em;
+        color: #666;
+    }
+
+    .phone-code .code-btn {
+        position: absolute;
+        top: 0;
+        right: 0;
+        color: #ed793d;
+        font-size: 16px;
+        width: 120px;
+        height: 44px;
+        line-height: 44px;
+        padding: 0 16px;
+        text-align: center;
+        cursor: pointer;
+    }
+
+    .phone-code .code-btn:before {
+        content: '';
+        position: absolute;
+        left: 0;
+        top: 4px;
+        height: 36px;
+        width: 1px;
+        background-color: #ddd;
     }
 </style>
