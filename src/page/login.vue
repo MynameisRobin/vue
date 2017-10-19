a3d633b81b5211ed8300c49cbb805b9466c44124
Robin Qiu <574244038@qq.com>
2017-10-19 09:46:28 +0800

    修改maxlength写法

--- src/page/login.vue
+++ src/page/login.vue
@@ -12,17 +12,17 @@
                         <h2>登录</h2>
                     </div>
                     <el-form-item label="" prop="usercode">
-                        <el-input v-model="loginForm.usercode" placeholder="手机号"></el-input>
+                        <el-input v-model="loginForm.usercode" placeholder="手机号" :maxlength="11"></el-input>
                     </el-form-item>
                     <el-form-item label="" prop="password">
                         <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="密码" @keyup.13="login"></el-input>
                     </el-form-item>
                     <div class="action">
-                        <div class="clearfix">  
+                        <div class="clearfix">
                             <input id="remember" class="remember" type="checkbox" name="remember" checked="checked" v-model="checked" @click="submitForm('loginForm')">
-                            <label for="remember">一个月内免登录</label>
+                            <label for="remember">一个月内免登录</label> 
                             <label class="f-right">
-                                <a class="linelight" href="forget.html">忘记密码</a>							
+                                <a class="linelight" href="forget.html">忘记密码</a>
                             </label>
                         </div>
                     </div>
@@ -33,198 +33,189 @@
                     </div>
                 </el-form>
             </div>
-        </div> 
+        </div>
     </div>
 </template>
 
 <script>
-    import '../css/reset.css'
-    import '../css/zx.css'
-    import {is_mobile,isNull} from '../js/util.js'
-    import Api from '../api/api.js'
-    let api = new Api();
-    export default {
-        name: 'Login',
-        data() {
-            let isMobile = (rule, value, callback) =>{
-                if (isNull(value)) {
-                    callback(new Error('请输入手机号'));
-                } 
-                else{
-                    console.log(is_mobile(value))if (is_mobile(value)) {
-                        this.$refs.loginForm.validateField('usercode');
-                    }else{
-                        callback(new Error('手机号格式不正确'));
-                    }
-                    callback();
-                }
-            };
-            return {
-                checked:true,
-                loginForm: {
-                    usercode:'',
-                    password:'',
-                },
-                rules: {
-                    usercode: [
-                        { validator:isMobile,trigger:'blur'}
-                    ],
-                    password:[
-                        { required: true, message: '请输入密码', trigger: 'blur' }
-                    ]
-                }
+import '../css/reset.css'
+import '../css/zx.css'
+import { is_mobile, isNull } from '../js/util.js'
+import Api from '../api/api.js'
+let api = new Api();
+export default {
+    name: 'Login',
+    data() {
+        // 手机验证方法
+        let isMobile = (rule, value, callback) => {
+            if (isNull(value)) {
+                callback(new Error('请输入手机号'));
             }
-        },
-        methods:{
-            submitForm(formName) {
-                this.$refs[formName].validate((valid) => {
-                    if (valid) {
-                        api.reqNewAjaxAsync(api.apiUrl.login,{
-                            password:this.loginForm.password,
-                            usercode:this.loginForm.usercode,
-                        })
-                        .then((res) =>{
-                            if(res.code == 1){
+            else if (!is_mobile(value)) {
+                callback(new Error('手机号格式不正确'));
+            } else {
+                callback();
+            }
+        }
+        return {
+            checked: true,
+            loginForm: {
+                usercode: '',//用户名
+                password: '',//密码
+            },
+            rules: {
+                usercode: [
+                    { validator: isMobile, trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: '请输入密码', trigger: 'blur' }
+                ]
+            }
+        }
+    },
+    methods: {
+        // 提交form表单
+        submitForm(formName) {
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    api.reqNewAjaxAsync(api.apiUrl.login, {
+                        password: this.loginForm.password,
+                        usercode: this.loginForm.usercode,
+                    })
+                        .then((res) => {
+                            if (res.code == 1) {
                                 // this.$router.to('backend')
-                            }else{
+                            } else {
                                 this.$notify.error({
                                     title: '错误',
                                     message: '这是一条错误的提示消息'
                                 });
                             }
                         })
-                    } else {
-                        console.log('error submit!!');
-                        return false;
-                    }
-                });
-            },
-            // resetForm(formName) {
-            //     this.$refs[formName].resetFields();
-            // },
-            // isCheck(){
-            //     if(!is_mobile(this.$data.usercode) || isNull(this.$data.usercode)){
-            //         this.status = false;
-            //         this.$message.error('手机号格式不正确');
-            //         this.tip = '手机号格式不正确';
-            //         return false;
-            //     }
-            //     else if(isNull(this.$data.password)){
-            //         this.status = false;
-            //         this.$message.error('密码格式不正确');
-            //         this.tip = '密码格式不正确';
-            //         return false;
-            //     }
-            //     this.status = true;
-            // },
-            // login() {
-            //     var _this = this;
-            //     this.isCheck();
-            //     if(!this.status){
-            //         return false;
-            //     }
-            // }
-            
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
+            });
         },
-        mounted(){
-            // this.isCheck();
-            // var isLogin = true
-            // if (isLogin) {
-            //     this.$router.replace('backend')
-            // }
-        }
+        // resetForm(formName) {
+        //     this.$refs[formName].resetFields();
+        // },
+        // isCheck(){
+        //     if(!is_mobile(this.$data.usercode) || isNull(this.$data.usercode)){
+        //         this.status = false;
+        //         this.$message.error('手机号格式不正确');
+        //         this.tip = '手机号格式不正确';
+        //         return false;
+        //     }
+        //     else if(isNull(this.$data.password)){
+        //         this.status = false;
+        //         this.$message.error('密码格式不正确');
+        //         this.tip = '密码格式不正确';
+        //         return false;
+        //     }
+        //     this.status = true;
+        // },
+        // login() {
+        //     var _this = this;
+        //     this.isCheck();
+        //     if(!this.status){
+        //         return false;
+        //     }
+        // }
+
+    },
+    mounted() {
+        // this.isCheck();
+        // var isLogin = true
+        // if (isLogin) {
+        //     this.$router.replace('backend')
+        // }
     }
+}
 </script>
 
 <style lang="less" scope>
-    .btn-block{
-        display:block;
-    }
-    .btn-lg{
-        padding: 10px 16px;
-        font-size: 18px;
-        line-height: 1.3333333;
-        border-radius: 6px;
-    }
-    .wfull{
-        width:100%;
-        height:100%;
-    }
-    .wrapper {
-        position: relative;
-        background-image: url(../images/login_bg.jpg);
-        background-repeat: no-repeat;
-        background-position: top center;
-    }
+.btn-block {
+    display: block;
+}
 
-    h2 {
-        font-size: 24px;
-        color: #ff8b6f;
-        text-align: center;
-    }
+.btn-lg {
+    padding: 10px 16px;
+    font-size: 18px;
+    line-height: 1.3333333;
+    border-radius: 6px;
+}
 
-    .input-field {
-        position: relative;
-        font-size: 16px;
-        margin-bottom: 14px;
-        color: #666;
-    }
+.wfull {
+    width: 100%;
+    height: 100%;
+}
 
-    .base-register .input-field {
-        margin-bottom: 14px;
-    }
+.wrapper {
+    position: relative;
+    background-image: url(../images/login_bg.jpg);
+    background-repeat: no-repeat;
+    background-position: top center;
+}
 
-    .action {
-        margin-bottom: 14px;
-    }
+h2 {
+    font-size: 24px;
+    color: #ff8b6f;
+    text-align: center;
+}
 
-    .action .remember {
-        vertical-align: baseline;
-        margin-right: 4px;
-    }
+.action {
+    margin-bottom: 14px;
+}
 
-    .action label {
-        color: #999999;
-        font-weight: 400;
-        font-size: 16px;
-    }
+.action .remember {
+    vertical-align: baseline;
+    margin-right: 4px;
+}
 
-    .linelight {
-        font-size: 16px;
-        color: #ff8b6f;
-    }
+.action label {
+    color: #999999;
+    font-weight: 400;
+    font-size: 16px;
+}
 
-    .linelight:hover {
-        text-decoration: underline;
-    }
+.linelight {
+    font-size: 16px;
+    color: #ff8b6f;
+}
 
-    .relex {
-        margin-top: 15px;
-        line-height: 1em;
-        color: #666;
-    }
+.linelight:hover {
+    text-decoration: underline;
+}
 
-    .phone-code .code-btn {
-        position: absolute;
-        top: 0;
-        right: 0;
-        color: #ed793d;
-        font-size: 16px;
-        width: 120px;
-        height: 44px;
-        line-height: 44px;
-        padding: 0 16px;
-        text-align: center;
-        cursor: pointer;
-    }
+.relex {
+    margin-top: 15px;
+    line-height: 1em;
+    color: #666;
+}
 
-    .phone-code .code-btn:before {
-        content: '';
-        position: absolute;
-        left: 0;
-        top: 4px;
-        height: 36px;
-        width: 1px;
-        background-color: #ddd;
-    }
+.phone-code .code-btn {
+    position: absolute;
+    top: 0;
+    right: 0;
+    color: #ed793d;
+    font-size: 16px;
+    width: 120px;
+    height: 44px;
+    line-height: 44px;
+    padding: 0 16px;
+    text-align: center;
+    cursor: pointer;
+}
+
+.phone-code .code-btn:before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 4px;
+    height: 36px;
+    width: 1px;
+    background-color: #ddd;
+}
 </style>
